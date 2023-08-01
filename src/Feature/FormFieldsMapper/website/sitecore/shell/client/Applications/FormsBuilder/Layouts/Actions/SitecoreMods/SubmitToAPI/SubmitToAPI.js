614cb561ff672d4c51f73a8cae04cb27147ee955
Arun P K <codeblitzmaster@gmail.com>
2023-08-01 22:04:00 +0530

    FormFieldsMapper Updates- Tabs

--- src/Feature/FormFieldsMapper/website/sitecore/shell/client/Applications/FormsBuilder/Layouts/Actions/SitecoreMods/SubmitToAPI/SubmitToAPI.js
+++ src/Feature/FormFieldsMapper/website/sitecore/shell/client/Applications/FormsBuilder/Layouts/Actions/SitecoreMods/SubmitToAPI/SubmitToAPI.js
@@ -19,13 +19,57 @@
 
                 setFormFields: function () {
                     var formFields = this.FormClientApi.getFields();
-                    this.FormFieldsMapper.setFormFields(formFields);
+                    debugger;
+                    this.MapFieldsTabApp.FormFieldsMapper.setFormFields(formFields);
+                    this.MapFieldsTabApp.FormFieldsMapper.setDestinationFields(this.ApiEndpointTabApp.ApiEndpointTreeView.SelectedItem);
                 },
-
                 loadDone: function (parameters) {
-                    this.setFormFields();
+                    var app = this;
+                    //this.Tabs.on("loaded", function () {
+                    //    debugger;
+                    //});
+                    this.Tabs.on("loaded:ApiEndpointTab", function () {
+                        app.bindApiEndpointEvents();
+                    });
+                    this.Tabs.on("loaded:MapFieldsTab", function () {
+                        app.setFormFields();
+                        debugger
+                    });
+                    this.Tabs.on("loaded:ReviewTab", function () {
+                        debugger;
+                    });
+                    debugger;
+                    //this.setFormFields();
                     this.Parameters = parameters || {};
+                },
+                bindApiEndpointEvents: function () {
+                    this.ApiEndpointTabApp.ApiEndpointTreeView.on("change:SelectedItem", this.changedSelectedItemId, this);
+                },
+                changedSelectedItemId: function () {
+                    var selectedItem = this.ApiEndpointTabApp.ApiEndpointTreeView.SelectedItem;
+                    var isSelectable = !!selectedItem;
+
+                    var mapFieldsTab = this.getTab("MapFieldsTab");
+                    //enabling MapFieldsTab (Mappings)
+                    if (isSelectable && selectedItem.$templateName == "ApiIntegration") {
+                        //this.Tabs.toggleEnabledAt(1);
+                        mapFieldsTab.IsDisabled = 0;
+                        debugger;
+                        //logic to enable ok button
+                        //parentApp.setSelectability(this, isSelectable, this.ApiEndpointTabApp.ApiEndpointTreeView.SelectedItemId);
+                    }
+                    else {
+                        mapFieldsTab.IsDisabled = 1;
+                    }
+                },
+                getTab: function (name) {
+                    var tab = _.filter(this.Tabs.Items, function (item) {
+                        return item.$itemName === name;
+                    });
+
+                    return tab[0];
                 }
+
             };
         });
 })(Sitecore.Speak);
--- src/Feature/FormFieldsMapper/website/sitecore/shell/client/Applications/FormsBuilder/Layouts/Renderings/SitecoreMods/FormFieldsMapper/FormFieldsMapper.js
+++ src/Feature/FormFieldsMapper/website/sitecore/shell/client/Applications/FormsBuilder/Layouts/Renderings/SitecoreMods/FormFieldsMapper/FormFieldsMapper.js
@@ -14,19 +14,91 @@
 ////})(Sitecore.Speak);
 
 (function (Speak) {
-    Speak.component({
-        initialize: function () {
-            debugger;
-            this.defineProperty("FormFields", []);
-        },
-        setFormFields: function (formFields) {
-            this.FormFields = formFields;
-            this.renderFormFields();
-        },
-        renderFormFields: function () {
-            var formFieldsList = '';
-            $(this.FormFields).each(function () { formFieldsList += '<li>' + this.name + ' (' + this.itemId +')'+'</li>'; });
-            $('.sc-FormFieldsMapper[data-sc-id="' + this.id + '"] .debug-info').html('<ul>' + formFieldsList + '</ul>');
-        }
-    }, "FormFieldsMapper");
-})(Sitecore.Speak);
+
+    Speak.component(["itemJS"],
+        function (itemManager) {
+            var getItem = function (guid, callBack, options) {
+                if (!guid) {
+                    callBack(null);
+                    return;
+                }
+
+                options = options || {};
+                options.language = Speak.Context.current().Language;
+                options.database = Speak.Context.current().contentDatabase;
+
+                itemManager.fetch(guid,
+                    options,
+                    function (data, result) {
+                        if (result.statusCode === 401) {
+                            Speak.module("bclSession").unauthorized();
+                            return;
+                        }
+                        callBack(data);
+                    });
+            };
+            var getChildren = function (guid, callBack, options) {
+                if (!guid) {
+                    callBack(null);
+                    return;
+                }
+
+                options = options || {};
+                options.language = Speak.Context.current().Language;
+                options.database = Speak.Context.current().contentDatabase;
+
+                itemManager.fetchChildren(guid, options, function (data, result) {
+                    if (result.statusCode === 401) {
+                        Speak.module("bclSession").unauthorized();
+                        return;
+                    }
+                    console.log(data);
+                    callBack(data);
+                });
+            };
+
+            var getListItems =
+                function (listItems, valueFieldName, displayFieldName, selectedValue, notFoundText) {
+                    var currentField = {};
+                    currentField[valueFieldName] = selectedValue;
+                    if (selectedValue && !_.findWhere(listItems, currentField)) {
+                        currentField[displayFieldName] = selectedValue + " - " + notFoundText;
+                        currentField.notFound = true;
+                        var items = listItems.slice(0);
+                        items.unshift(currentField);
+
+                        return items;
+                    }
+
+                    return listItems;
+                };
+
+            return Speak.extend({}, {
+                initialize: function () {
+                    debugger;
+                    this.defineProperty("FormFields", []);
+                    this.defineProperty("DestinationFields", []);
+
+                },
+                setFormFields: function (formFields) {
+                    this.FormFields = formFields;
+                    this.renderFormFields();
+                },
+                setDestinationFields: function (item) {
+                    options = {};
+                    options.database = Speak.Context.current().contentDatabase;
+                    getChildren(item.$itemId, function (items) {
+                        debugger;
+                        this.DestinationFields = (items.length > 0) ? items : [];
+                    }.bind(this));
+                    
+                    debugger;
+                },
+                renderFormFields: function () {
+                    var formFieldsList = '';
+                    $(this.FormFields).each(function () { formFieldsList += '<li>' + this.name + ' (' + this.itemId + ')' + '</li>'; });
+                    $('.sc-FormFieldsMapper[data-sc-id="' + this.id + '"] .debug-info').html('<ul>' + formFieldsList + '</ul>');
+                }
+            });
+        }, "FormFieldsMapper");
+}) (Sitecore.Speak);
